ee4d6c7d042a47cb67447c30abbd8790d77cee13
khuerecak <khuerecak@gmail.com>
2023-03-21 01:06:26 -0500

    working with port issue

--- config/Database.php
+++ config/Database.php
@@ -6,21 +6,20 @@ class Database{
 	private $password;
 	private $dbname;
 	private $host;
-	private $port;
+	
 
 	public function __construct(){
 		$this->username = getenv('USERNAME');
 		$this->password = getenv('PASSWORD');
 		$this->dbname = getenv('DBNAME');
 		$this->host = getenv('HOST');
-		$this->port = getenv('PORT');
 	}
 	
 	//DB Connect
 	public function connect(){
 		$this->conn = null;
 
-		$dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->dbname}";
+		$dsn = "pgsql:host={$this->host};dbname={$this->dbname}";
 		try{
 			$this->conn = new PDO($dsn, $this->username, $this->password);
 			$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
