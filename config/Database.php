402977a72257458390a7b8e3ac8b041d18d255b8
khuerecak <khuerecak@gmail.com>
2023-03-21 01:43:25 -0500

    please connect db

--- config/Database.php
+++ config/Database.php
@@ -1,33 +1,37 @@
 <?php
-class Database{
-	//DB Params
-	private $conn; 
-	private $username;
-	private $password;
-	private $dbname;
-	private $host;
-	private $port;
+    class Database {
+        //DB params 
+        private $host;
+        private $port;
+        private $dbname;
+        private $username;
+        private $password;
+        private $conn;
 
-	public function __construct(){
-		$this->username = getenv('USERNAME');
-		$this->password = getenv('PASSWORD');
-		$this->dbname = getenv('DBNAME');
-		$this->host = getenv('HOST');
-		$this->port = getenv('PORT');
-	}
-	
-	//DB Connect
-	public function connect(){
-		$this->conn = null;
 
-		$dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->dbname}";
-		try{
-			$this->conn = new PDO($dsn, $this->username, $this->password);
-			$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	}catch(PDOException $e){
-		echo 'Connection error: ' . $e->getMessage();
-	}
+         public function __construct() {
+             $this->username = getenv('USERNAME');
+             $this->password = getenv('PASSWORD');
+             $this->dbname = getenv('DBNAME');
+             $this->host = getenv('HOST');
+             $this->port = getenv('PORT');
 
-	return $this->conn;
-	}
-}
+         }
+         
+        //DB connect 
+         public function connect () {
+             if ($this->conn) {
+                 return $this->conn;
+             } else {
+                 $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->dbname};";
+
+                 try {
+                     $this->conn = new PDO($dsn, $this->username, $this->password);
+                     $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                     return $this->conn;  
+                 } catch(PDOException $e) {
+                     echo 'Connection Error: ' . $e->getMessage();
+                 }
+             }
+         }
+    }
