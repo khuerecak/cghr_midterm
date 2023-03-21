b59dca4ee3063d6867ce1a1f0ce263da170812b4
khuerecak <khuerecak@gmail.com>
2023-03-20 23:17:13 -0500

    db issues

--- config/Database.php
+++ config/Database.php
@@ -1,39 +1,34 @@
 <?php
-    class Database {
+class Database{
+	//DB Params
+	private $conn; 
+	private $username;
+	private $password;
+	private $dbname;
+	private $host;
+	private $port;
+
+	public function __construct(){
+		$this->username = getenv('USERNAME');
+		$this->password = getenv('PASSWORD');
+		$this->dbname = getenv('DBNAME');
+		$this->host = getenv('HOST');
+		$this->port = getenv('PORT');
+	}
+	
+	//DB Connect
+	public function connect(){
+		$this->conn = null;
+
+		$dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->dbname}";
+		try{
+			$this->conn = new PDO($dsn, $this->username, $this->password);
+			$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	}catch(PDOException $e){
+		echo 'Connection error: ' . $e->getMessage();
+	}
+
+	return $this->conn;
+	}
+}
     
-        //DB params 
-        private $conn;
-        private $host;
-        private $port;
-        private $dbname;
-        private $username;
-        private $password;
-        
-
-
-         public function __construct() {
-             $this->username = getenv('USERNAME');
-             $this->password = getenv('PASSWORD');
-             $this->dbname = getenv('DBNAME');
-             $this->host = getenv('HOST');
-             $this->port = getenv('PORT');
-
-         }
-         
-        //DB connect 
-         public function connect () {
-             if ($this->conn) {
-                 return $this->conn;
-             } else {
-                 $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->dbname};";
-
-                 try {
-                     $this->conn = new PDO($dsn, $this->username, $this->password);
-                     $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                     return $this->conn;  
-                 } catch(PDOException $e) {
-                     echo 'Connection Error: ' . $e->getMessage();
-                 }
-             }
-         }   
-    }
---
+++ replit.nix
@@ -0,0 +1,1 @@
+{ pkgs }: { deps = [ pkgs.php82 ]; }
